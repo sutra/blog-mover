04c990817a4d0182eda1cc1dcfa10292c6648fb5
Rory Ye <rory.cn@gmail.com>
2006-10-19 02:53:56 +0000

    add Simple Test

---
+++ src/test/java/com/redv/blogmover/simple/SimpleTest.java
@@ -0,0 +1,30 @@
+/**
+ * <pre>
+ * Copyright:		Copyright(C) 2005-2006, feloo.org
+ * Filename:		SimpleTest.java
+ * Module:			blog-mover
+ * Class:			SimpleTest
+ * Date:			19/10/2006 10:53:12 AM
+ * Author:			<a href="mailto:rory.cn@gmail.com">somebody</a>
+ * Description:		
+ *
+ *
+ * ======================================================================
+ * Change History Log
+ * ----------------------------------------------------------------------
+ * Mod. No.	| Date		| Name			| Reason			| Change Req.
+ * ----------------------------------------------------------------------
+ * 			| 19/10/2006   | Rory Ye	    | Created			|
+ *
+ * </pre>
+ **/
+package com.redv.blogmover.simple;
+
+/**
+ * @author <a href="rory.cn@gmail.com">somebody</a>
+ * @since 19/10/2006 10:53:12 AM
+ * @version $Id: SimpleTest.java,v Exp $
+ */
+public class SimpleTest {
+
+}
