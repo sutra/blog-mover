4fe893f24fcbeb20297bf9d198df56e3892cb2f3
Joe Zhou <zhoushuqun@gmail.com>
2007-01-17 09:15:59 +0000

    Add tag implementation.

---
+++ src/main/java/com/redv/blogmover/web/tags/ReportTag.java
@@ -0,0 +1,106 @@
+/**
+ * Created on 2007-1-17 下午04:09:26
+ */
+package com.redv.blogmover.web.tags;
+
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.Map;
+
+import javax.servlet.jsp.JspException;
+import javax.servlet.jsp.JspWriter;
+import javax.servlet.jsp.tagext.TagSupport;
+
+import org.springframework.web.context.support.WebApplicationContextUtils;
+
+import com.redv.blogmover.logging.BSP;
+import com.redv.blogmover.logging.ReportBuilder;
+
+/**
+ * @author shutra
+ * 
+ */
+public class ReportTag extends TagSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 744369341150491624L;
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see javax.servlet.jsp.tagext.TagSupport#doEndTag()
+	 */
+	@Override
+	public int doEndTag() throws JspException {
+		try {
+			render();
+		} catch (IOException e) {
+			throw new JspException(e);
+		}
+		return super.doEndTag();
+	}
+
+	private void render() throws IOException {
+		JspWriter out = this.pageContext.getOut();
+		out
+				.println("<table style=\"border: 1px solid #333;background-color: #eee;\">");
+		out.println("<thead>");
+		out.println("<tr>");
+		out.println("<th>");
+		out.println("搬迁统计");
+		out.println("</th>");
+		out.println("</tr>");
+		out.println("</thead>");
+		out.println("<tbody>");
+		this.renderLines();
+		out.println("</tbody>");
+		out.println("<tfoot>");
+		out.println("<tr>");
+		out.println("<td>");
+		out
+				.println("<div class=\"movedIn\" style=\"width: 40px;display: inline;\">");
+		out.println("&nbsp;");
+		out.println("</div>");
+		out.println("搬入次数");
+		out
+				.println("<div class=\"movedOut\" style=\"width: 40px;display: inline;\">");
+		out.println("&nbsp;");
+		out.println("</div>");
+		out.println("搬出次数");
+		out.println("</td>");
+		out.println("</tr>");
+		out.println("</tfoot>");
+		out.println("</table>");
+	}
+
+	private void renderLines() throws IOException {
+		JspWriter out = this.pageContext.getOut();
+		ReportBuilder reportBuilder = (ReportBuilder) WebApplicationContextUtils
+				.getWebApplicationContext(this.pageContext.getServletContext())
+				.getBean("reportBuilder");
+		Map<BSP, long[]> statistic = reportBuilder.buildStatistic();
+		for (Iterator<BSP> iter = statistic.keySet().iterator(); iter.hasNext();) {
+			BSP bsp = iter.next();
+			long[] movedInOut = statistic.get(bsp);
+			long w0 = movedInOut[0];
+			long w1 = movedInOut[1];
+			out.print("<tr>");
+			out.print("<td>");
+			out.print(bsp.getId());
+			// <fmt:message key="com.redv.blogmover.bsps.${bsp.id}" />
+			out.print("</td>");
+			out.print("<td>");
+			out.print("<div class='movedIn' style='width:" + w0
+					+ "px;'>&nbsp;</div>");
+			out.print("<div class='movedOut' style='width:" + w1
+					+ "px;'>&nbsp;</div>");
+			out.print("</td>");
+			out.print("<td>" + w0 + "<br />" + w1 + "</td>");
+			out.print("</tr>\n");
+		}
+
+	}
+
+}
--- src/main/webapp/report.jsp
+++ src/main/webapp/report.jsp
@@ -1,13 +1,7 @@
 <jsp:directive.page contentType="text/html; charset=UTF-8" />
-<jsp:directive.page
-	import="org.springframework.web.context.support.WebApplicationContextUtils" />
-<jsp:directive.page import="com.redv.blogmover.logging.ReportBuilder" />
-<jsp:directive.page import="java.util.Iterator" />
-<jsp:directive.page import="java.util.Map" />
-<jsp:directive.page import="com.redv.blogmover.logging.BSP" />
-<jsp:directive.page import="org.apache.commons.lang.StringUtils" />
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <%@ taglib uri="http://java.sun.com/jsp/jstl/fmt" prefix="fmt"%>
+<%@ taglib prefix="blog-mover" uri="http://blogmover.redv.com/"%>
 <html>
 	<head>
 		<title>搬迁报告 - Blog Mover</title>
@@ -23,55 +17,6 @@
 	</style>
 	</head>
 	<body>
-		<table style="border: 1px solid #333;background-color: #eee;">
-			<thead>
-				<tr>
-					<th>
-						搬迁统计
-					</th>
-				</tr>
-			</thead>
-			<tbody>
-			<fmt:bundle basename="com.redv.blogmover.bsps.messages-bsp-names">
-				<%
-					ReportBuilder reportBuilder = (ReportBuilder) WebApplicationContextUtils.getWebApplicationContext(this.getServletContext()).getBean("reportBuilder");
-					Map<BSP, long[]> statistic = reportBuilder.buildStatistic();
-					for (Iterator<BSP> iter = statistic.keySet().iterator(); iter.hasNext(); ) {
-						BSP bsp = iter.next();
-						request.setAttribute("bsp", bsp);
-						long[] movedInOut = statistic.get(bsp);
-						long w0 = movedInOut[0];
-						long w1 = movedInOut[1];
-						out.print("<tr>");
-						out.print("<td>");
-						%>
-						<fmt:message key="com.redv.blogmover.bsps.${bsp.id}" />
-						<%
-						out.print("</td>");
-						out.print("<td>");
-						out.print("<div class='movedIn' style='width:" + w0  + "px;'>&nbsp;</div>");
-						out.print("<div class='movedOut' style='width:" + w1 + "px;'>&nbsp;</div>");
-						out.print("</td>");
-						out.print("<td>" + w0 + "<br />" + w1 + "</td>");
-						out.print("</tr>\n");
-						}
-				%>
-				</fmt:bundle>
-			</tbody>
-			<tfoot>
-				<tr>
-					<td>
-						<div class="movedIn" style="width: 40px;display: inline;">
-							&nbsp;
-						</div>
-						搬入次数
-						<div class="movedOut" style="width: 40px;display: inline;">
-							&nbsp;
-						</div>
-						搬出次数
-					</td>
-				</tr>
-			</tfoot>
-		</table>
+		<blog-mover:report />
 	</body>
 </html>
