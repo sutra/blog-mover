ae54c7b83146875576bb210a10a54729832c94fc
Joe Zhou <zhoushuqun@gmail.com>
2007-06-16 09:23:29 +0000

    Fixed login check of baidu.

--- src/main/java/com/redv/blogmover/bsps/baidu/BaiduLogin.java
+++ src/main/java/com/redv/blogmover/bsps/baidu/BaiduLogin.java
@@ -3,6 +3,11 @@
  */
 package com.redv.blogmover.bsps.baidu;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.transform.TransformerException;
+
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HeaderGroup;
 import org.apache.commons.httpclient.NameValuePair;
@@ -47,17 +52,87 @@ public class BaiduLogin {
 		hg.addHeader(new Header("Content-Type",
 				"application/x-www-form-urlencoded; charset=GB2312"));
 		Document document = httpDocument.post(action, parameters, hg);
-		NodeList anchors = document.getElementsByTagName("a");
+//		NodeList anchors = document.getElementsByTagName("a");
 		boolean ok = false;
-		for (int i = 0; i < anchors.getLength(); i++) {
-			String s = DomNodeUtils.getTextContent(anchors.item(i));
-			if (s != null && s.equalsIgnoreCase(username)) {
-				ok = true;
-				break;
+//		for (int i = 0; i < anchors.getLength(); i++) {
+//			String s = DomNodeUtils.getTextContent(anchors.item(i));
+//			if (s != null && s.equalsIgnoreCase(username)) {
+//				ok = true;
+//				break;
+//			}
+//		}
+		
+		NodeList scripts = document.getElementsByTagName("script");
+		if (scripts.getLength() > 0) {
+			try {
+				String s = DomNodeUtils.getXmlAsString(scripts.item(0));
+				if (s.indexOf("location.href=\"http://hi.baidu.com/\"") != -1) {
+					ok = true;
+				}
+			} catch (TransformerException e) {
+				log.error(e);
 			}
 		}
+
 		if (!ok) {
-			throw new LoginFailedException("登录失败，用户名或者密码错误。");
+			String errorString = "登录失败，用户名或者密码错误。（Blog mover 无法获知更多错误信息。）";
+			if (scripts.getLength() >= 4) {
+				try {
+					String s = DomNodeUtils.getXmlAsString(scripts.item(4));
+					Pattern pattern = Pattern.compile("switch\\(([\\d])+\\)");
+					Matcher matcher = pattern.matcher(s);
+					log.debug("s: " + s);
+					if (matcher.find()) {
+						int errorCode = Integer.parseInt(matcher.group(1));
+						errorString = getErrorString(errorCode, username);
+					}
+				} catch (TransformerException e) {
+					log.error(e);
+					errorString = "登录失败，用户名或者密码错误。（Blog mover 无法获知更多错误信息。）";
+				}
+			}
+			throw new LoginFailedException(errorString);
 		}
+	}
+	
+	private String getErrorString(int errorCode, String username) {
+		String err_str;
+		switch(errorCode)
+		{
+		        case 1: 
+		                err_str = "用户名格式错误，请重新输入";
+		                break;
+		        case 2:
+		                err_str = "用户“" + username + "”不存在";
+		                break;
+		        case 3:
+		                err_str = "";
+		                break;
+		        case 4:
+		                err_str = "登录密码错误，请重新输入";
+		                break;
+		        case 5:
+		                err_str = "今日登录次数过多";
+		                break;
+		        case 6:
+		                err_str = "验证码不匹配，请重新输入验证码";
+		                break;
+		        case 7:
+		                err_str = "登录时发生未知错误，请重新输入";
+		                break;
+		        case 8:
+		                err_str = "登录时发生未知错误，请重新输入";
+		                break;
+		        case 16:
+		                err_str = "对不起，您现在无法登录";
+		                break;
+		        case 256:
+		                err_str = "";
+		                break;
+		        default:
+		                err_str = "登录时发生未知错误，请重新输入";
+		                break;
+		}
+		return err_str;
 	}
 }
