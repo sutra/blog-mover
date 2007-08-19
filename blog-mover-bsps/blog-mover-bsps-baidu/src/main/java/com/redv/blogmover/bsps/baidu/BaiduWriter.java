3340dfd2a3dbf4e3268a2689a9513c4518f07a95
Joe Zhou <zhoushuqun@gmail.com>
2007-08-19 10:02:20 +0000

    To get baidu error message.

--- blog-mover-bsps/blog-mover-bsps-baidu/src/main/java/com/redv/blogmover/bsps/baidu/BaiduWriter.java
+++ blog-mover-bsps/blog-mover-bsps-baidu/src/main/java/com/redv/blogmover/bsps/baidu/BaiduWriter.java
@@ -6,11 +6,16 @@ package com.redv.blogmover.bsps.baidu;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.xml.transform.TransformerException;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HeaderGroup;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.NameValuePair;
+import org.apache.commons.lang.StringUtils;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
@@ -21,6 +26,8 @@ import com.redv.blogmover.BlogMoverException;
 import com.redv.blogmover.Comment;
 import com.redv.blogmover.WebLog;
 import com.redv.blogmover.impl.AbstractBlogWriter;
+import com.redv.blogmover.impl.WebLogImpl;
+import com.redv.blogmover.util.DomNodeUtils;
 import com.redv.blogmover.util.HttpDocument;
 
 /**
@@ -42,6 +49,9 @@ public class BaiduWriter extends AbstractBlogWriter {
 	private String password;
 
 	private String blogHandle;
+
+	private static final Pattern messagePattern = Pattern
+			.compile(".*writestr\\(\"(.*)\"\\);.*");
 
 	/**
 	 * 
@@ -131,14 +141,27 @@ public class BaiduWriter extends AbstractBlogWriter {
 				"application/x-www-form-urlencoded; charset=GB2312"));
 		Document document = httpDocument.post(action, parameters, hg);
 		NodeList scripts = document.getElementsByTagName("script");
+		String message = null;
 		String expectMessage = "恭喜,您的文章已经发表成功。";
 		boolean ok = false;
 		for (int i = 0; i < scripts.getLength(); i++) {
 			Element script = (Element) scripts.item(i);
+			if (log.isDebugEnabled()) {
+				try {
+					log.debug("node: " + DomNodeUtils.getXmlAsString(script));
+				} catch (TransformerException e) {
+					log.debug(e);
+				}
+			}
 			Node firstChild = script.getFirstChild();
 			if (firstChild != null) {
 				String actualMessage = firstChild.getNodeValue();
 				log.debug("actualMessage: " + actualMessage);
+				Matcher matcher = messagePattern.matcher(actualMessage);
+				if (matcher.find()) {
+					log.debug("found");
+					message = matcher.group(1);
+				}
 				if (actualMessage != null
 						&& actualMessage.indexOf(expectMessage) != -1) {
 					ok = true;
@@ -146,7 +169,8 @@ public class BaiduWriter extends AbstractBlogWriter {
 			}
 		}
 		if (!ok) {
-			throw new BlogMoverException("写入错误。");
+			throw new BlogMoverException("写入错误，百度的提示为： "
+					+ StringUtils.defaultString(message, ""));
 		}
 	}
 
@@ -170,5 +194,17 @@ public class BaiduWriter extends AbstractBlogWriter {
 	protected String writeAttachment(Attachment att) {
 		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public static void main(String[] args) throws BlogMoverException {
+		BaiduWriter bw = new BaiduWriter();
+		bw.setUsername("blogmover1");
+		bw.setPassword("blogmover1");
+		List<WebLog> webLogs = new ArrayList<WebLog>();
+		WebLog webLog = new WebLogImpl();
+		webLog.setTitle("title");
+		// webLog.setBody("body");
+		webLogs.add(webLog);
+		bw.write(webLogs);
 	}
 }
